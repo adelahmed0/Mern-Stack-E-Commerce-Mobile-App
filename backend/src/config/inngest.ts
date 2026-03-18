26ebc6a19b8a21497ebee10146ee1e40fb3096bd
Adel Ahmed <adel15952@gmail.com>
2026-03-18 23:35:03 +0200

    Add user creation logic to Inngest sync function with database integration and Clerk data mapping

--- backend/src/config/inngest.ts
+++ backend/src/config/inngest.ts
@@ -8,6 +8,19 @@ export const inngest = new Inngest({ id: "Mern-Stack-E-Commerce-Mobile-App" });
 const syncUser = inngest.createFunction(
    {id:"sync-user"},
    {event:"clerk/user.created"},
+   async ({event}) => {
+    await connectDB();
+    const {id,email_addresses,first_name,last_name,image_url} = event.data;
+    const newUser = {
+      clerkId:id,
+      email:email_addresses[0]?.email_address,
+      name:`${first_name} ${last_name}`|| "User",
+      imageUrl:image_url,
+      adresses:[],
+      wishlist:[],
+    }
+    await User.create(newUser);
+   }
 )
 
 
